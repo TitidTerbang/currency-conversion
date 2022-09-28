73b399721a56d668c43b9e9696290769640580bd
TitidTerbang <yudisbaek@gmail.com>
2022-09-29 02:28:32 +0700

    konversi selesai

--- currency-conversion-api.py
+++ currency-conversion-api.py
@@ -1,11 +1,17 @@
 import requests
 
+negara1 = str(input('Kode Negara Yang Mau Menukar: '))
+negara2 = str(input('Kode Negara Yang Ditukarkan: '))
+jumlah = float(input('Jumlah Uang Yang Ditukarkan: '))
+
 key = '64e226ee33ac6ebdfba50575943e071754650daf'
 
-parameters = {"api_key": {key}, "from": "EUR", "to": "IDR", "amount": 1, "format": "json"}
+parameters = {"api_key": {key}, "from": {negara1}, "to": {negara2}, "amount": {jumlah}, "format": "json"}
 
 url = "https://api.getgeoapi.com/v2/currency/convert"
 
 response = requests.get(url, parameters)
+
+
 
 print(response.json())
