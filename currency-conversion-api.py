8d57e75455e0b1a1f72270eb7386315a4f90dcf0
TitidTerbang <yudisbaek@gmail.com>
2022-10-01 19:01:09 +0700

    input negara & jumlah uang, penjelasan konversi detail

--- currency-conversion-api.py
+++ currency-conversion-api.py
@@ -1,8 +1,11 @@
 import requests
+import json
+from addict import Dict
+
 
 negara1 = str(input('Kode Negara Yang Mau Menukar: '))
 negara2 = str(input('Kode Negara Yang Ditukarkan: '))
-jumlah = float(input('Jumlah Uang Yang Ditukarkan: '))
+jumlah = int(input('Jumlah Uang Yang Ditukarkan: '))
 
 key = '64e226ee33ac6ebdfba50575943e071754650daf'
 
@@ -11,7 +14,9 @@ parameters = {"api_key": {key}, "from": {negara1}, "to": {negara2}, "amount": {j
 url = "https://api.getgeoapi.com/v2/currency/convert"
 
 response = requests.get(url, parameters)
+data = response.json()
+dict = Dict(data)
 
-
-
-print(response.json())
+# print(data)
+# print(data['rates']['SGD']['rate']) <-- contoh cara mengambil data
+print('Dari', data.get('amount'), data.get('base_currency_code'), 'Anda Mendapatkan', data['rates']['SGD']['rate_for_amount'], data['rates']['SGD']['currency_name'])
